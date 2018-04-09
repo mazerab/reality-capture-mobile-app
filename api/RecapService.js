c79715fc51941afb94c432e017b47b77a9690556
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-09 12:10:09 -0700

    Update RecapService.js

--- api/RecapService.js
+++ api/RecapService.js
@@ -5,79 +5,25 @@ import Utils from './Utils';
 
 export default class RecapService {
 
-    constructor(accessToken) {
-        this.token = accessToken;
+    constructor() {
         this.utils = new Utils();
     }
 
-    async addImageAsync(s3Url) {
-        const endpoint = Config.AWS_LAMBDA_BASE_ENDPOINT + '/recap/addImage';
-        const requestBody = { file: s3Url };
-        const api = '/demo/recap/addImage';
-        this.utils.logRequestInfoToConsole(api, 'POST', endpoint, requestBody);
-        return fetch(endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(requestBody)
-        })
-            .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'POST', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to add image!', res);
-                    alert('Failed to add image!');
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
-
-    async checkDownloadSizeAsync() {
-        const endpoint = Config.AWS_LAMBDA_BASE_ENDPOINT + '/recap/checkOutputFileExists';
-        const api = '/demo/recap/checkOutputFileExists';
+    async pollProcessingStatus() {
+        const endpoint = Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT + '/redis/processingstatus';
+        const api = '/demo/redis/processingstatus';
         this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
         return fetch(endpoint, {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
         })
             .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'GET', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to check download file size!', res);
-                    alert('Failed to check download file size!');
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
-
-    async createPhotoSceneAsync() {
-        const endpoint = Config.AWS_LAMBDA_BASE_ENDPOINT + '/recap/createPhotoScene';
-        let requestBody = {
-            scenename: Config.sceneName,
-            scenetype: Config.sceneType,
-            format: Config.format,
-            callback: Config.AWS_LAMBDA_BASE_ENDPOINT + Config.sceneCallback     
-        };
-        const api = '/demo/recap/createPhotoScene';
-        this.utils.logRequestInfoToConsole(api, 'POST', endpoint, requestBody);
-        return fetch(endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(requestBody)
-        })
-            .then((res) => {
                 this.utils.logResponseInfoToConsole(api, 'POST', res);
                 if (res.ok) {
                     return res.json();
                 } else {
-                    this.utils.logResponseErrorToConsole('Failed to create photoscene!', res);
-                    alert('Failed to create photoscene!');
+                    this.utils.logResponseErrorToConsole('Failed to set processing status!', res);
+                    alert('Failed to set processing status!');
                 }
             })
             .catch((err) => {
@@ -85,49 +31,8 @@ export default class RecapService {
             });
     }
 
-    async downloadProcessedDataAsync() {
-        const endpoint = Config.AWS_LAMBDA_BASE_ENDPOINT + '/recap/downloadProcessedData?format=' + Config.format;
-        const api = '/demo/recap/downloadProcessedData';
-        this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
-        return fetch(endpoint, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then( (res) => {
-                this.utils.logResponseInfoToConsole(api, 'GET', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to download processed data!', res);
-                    alert('Failed to download processed data!');
-                }
-            }).catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
-
-    async pollPhotosceneProcessingProgress() {
-        const endpoint = Config.AWS_LAMBDA_BASE_ENDPOINT + '/recap/scene/processingStatus';
-        const api = '/demo/recap/scene/processingStatus';
-        this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
-        return fetch(endpoint, {
-            method: 'GET',
-        })
-            .then( (res) => {
-                this.utils.logResponseInfoToConsole(api, 'GET', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to retrieve processing progress!', res);
-                    alert('Failed to retrieve processing progress!');
-                }
-            }).catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
-
-    async processPhotoSceneAsync() {
-        const endpoint = Config.AWS_LAMBDA_BASE_ENDPOINT + '/recap/processPhotoScene';
+    async processPhotoScene() {
+        const endpoint = Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT + '/recap/processPhotoScene';
         const api = '/demo/recap/processPhotoScene';
         this.utils.logRequestInfoToConsole(api, 'POST', endpoint, null);
         return fetch(endpoint, {
@@ -139,8 +44,8 @@ export default class RecapService {
                 if (res.ok) {
                     return res.json();
                 } else {
-                    this.utils.logResponseErrorToConsole('Failed to process photoscene!', res);
-                    alert('Failed to process photoscene!');
+                    this.utils.logResponseErrorToConsole('Failed to process Photoscene!', res);
+                    alert('Failed to process Photoscene!');
                 }
             })
             .catch((err) => {
@@ -148,21 +53,21 @@ export default class RecapService {
             });
     }
 
-    async processPhotoSceneResetAsync() {
-        const endpoint = Config.AWS_LAMBDA_BASE_ENDPOINT + '/recap/scene/processingReset';
-        const api = '/demo/recap/scene/processingReset';
-        this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
+    async setProcessingStatusInProgress() {
+        const endpoint = Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT + '/redis/processingstatus?processingstatus=InProgress';
+        const api = '/demo/redis/processingstatus';
+        this.utils.logRequestInfoToConsole(api, 'POST', endpoint, null);
         return fetch(endpoint, {
-            method: 'GET',
+            method: 'POST',
             headers: { 'Content-Type': 'application/json' }
         })
-            .then( (res) => {
-                this.utils.logResponseInfoToConsole(api, 'GET', res);
+            .then((res) => {
+                this.utils.logResponseInfoToConsole(api, 'POST', res);
                 if (res.ok) {
                     return res.json();
                 } else {
-                    this.utils.logResponseErrorToConsole('Failed to reset processing progress!', res);
-                    alert('Failed to reset processing progress!');
+                    this.utils.logResponseErrorToConsole('Failed to set processing status!', res);
+                    alert('Failed to set processing status!');
                 }
             })
             .catch((err) => {
