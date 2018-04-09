f1529b0930a5cbc7301f79b252e6b91b5612de2f
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-09 12:08:58 -0700

    Update DerivativeService.js

--- api/DerivativeService.js
+++ api/DerivativeService.js
@@ -9,43 +9,21 @@ export default class DerivativeService {
         this.utils = new Utils();
     }
 
-    async getManifest(urn) {
-    	const endpoint = Config.AWS_LAMBDA_BASE_ENDPOINT + '/derivative/getStatus?urn=' + urn;
-    	const api = '/demo/derivative/getStatus';
-    	this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
+    async getManifest() {
+        const endpoint = Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT + '/derivative/getManifest';
+        const api = '/demo/derivative/getManifest';
+        this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
         return fetch(endpoint, {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
         })
             .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'POST', res);
+                this.utils.logResponseInfoToConsole(api, 'GET', res);
                 if (res.ok) {
                     return res.json();
                 } else {
-                    this.utils.logResponseErrorToConsole('Failed to get translation status!', res);
-                    alert('Failed to get translation status!');
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
-
-    async translateSourceToSVF(objectId) {
-    	const endpoint = Config.AWS_LAMBDA_BASE_ENDPOINT + '/derivative/translateToSVF?objectid=' + objectId;
-        const api = '/demo/derivative/translateToSVF';
-        this.utils.logRequestInfoToConsole(api, 'POST', endpoint, null);
-        return fetch(endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'POST', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to start SVF translation!', res);
-                    alert('Failed to start SVF translation!');
+                    this.utils.logResponseErrorToConsole('Failed to get manifest!', res);
+                    alert('Failed to get manifest!');
                 }
             })
             .catch((err) => {
