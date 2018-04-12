facad057d2d610f6ce45cca2e2a911e318f1852c
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-12 14:26:56 -0700

    Update ImageScreen.js

--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -189,10 +189,11 @@ export default class ImageScreen extends React.Component {
   };
 
   uploadAndTranslate = async () => {
+    this.setState({ processing: true, processButtonDisabled: true });
     this.DataService = new DataService();
     this.DerivativeService = new DerivativeService();
     const uploadResult = await this.DataService.uploadAndTranslateProcessedData();
-    if(uploadResult.statusCode === 200) { 
+    if(uploadResult && uploadResult.statusCode === 200) { 
       let manifestStatus = 'notstarted';
       let urn;
       const intervalId = setInterval(async () => {
@@ -203,17 +204,16 @@ export default class ImageScreen extends React.Component {
               manifestStatus = manifestResult.status;
               urn = manifestResult.urn;
             }
-          if (manifestStatus === 'success' && !this.manifestStatusIgnore)
-            if(urn) {
-              this.manifestStatusIgnore = true;
-              this.setState({ viewFileButtonDisabled: false, urn: urn });
-              clearInterval(this.state.processTranslationIntervalId);
-            }
+          }
+          if (manifestStatus === 'success' && !this.manifestStatusIgnore && urn) {
+            this.manifestStatusIgnore = true;
+            this.setState({ processing: false, urn: urn, viewFileButtonDisabled: false });
+            clearInterval(this.state.processTranslationIntervalId);
           }
         }
       }, 1000);
       this.setState({ processTranslationIntervalId: intervalId });
-    };
+    }
   };
 
 }
