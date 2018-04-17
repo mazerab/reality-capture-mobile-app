2a27de491e08dcc59555a01af05b037731529343
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-17 16:13:56 -0700

    Update DerivativeService.js

--- api/DerivativeService.js
+++ api/DerivativeService.js
@@ -9,6 +9,28 @@ export default class DerivativeService {
         this.utils = new Utils();
     }
 
+    async getDerivativeUrn() {
+        const endpoint = Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT + '/derivative/getDerivativeUrn';
+        const api = '/demo/derivative/getDerivativeUrn';
+        this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
+        return fetch(endpoint, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then((res) => {
+                this.utils.logResponseInfoToConsole(api, 'GET', res);
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    this.utils.logResponseErrorToConsole('Failed to get derivative urn!', res);
+                    alert('Failed to get derivative urn!');
+                }
+            })
+            .catch((err) => {
+                this.utils.logFetchErrorToConsole(err);
+            });
+    }
+
     async getManifest() {
         const endpoint = Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT + '/derivative/getManifest';
         const api = '/demo/derivative/getManifest';
