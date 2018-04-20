f55db8aa0fbe145d99cf4394e2bb18e5caaa2ca8
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-20 14:44:59 -0700

    Update MainTabNavigator.js

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -9,43 +9,43 @@ import ImageScreen from '../screens/ImageScreen';
 import ViewerScreen from '../screens/ViewerScreen';
 
 export default TabNavigator(
-  {
-    Images: {
-      screen: ImageScreen,
+    {
+        Images: {
+            screen: ImageScreen,
+        },
+        ForgeViewer: {
+            screen: ViewerScreen,
+        },
     },
-    Viewer: {
-      screen: ViewerScreen,
-    },
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        switch (routeName) {
-          case 'Images':
-            iconName =
-              Platform.OS === 'ios'
-                ? `ios-camera${focused ? '' : '-outline'}`
-                : 'md-camera';
-            break;
-          case 'Viewer':
-            iconName = Platform.OS === 'ios' ? `ios-cloud-done${focused ? '' : '-outline'}` : 'md-cloud-done';
-            break;
-        }
-        return (
-          <Ionicons
-            name={iconName}
-            size={28}
-            style={{ marginBottom: -3 }}
-            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
-          />
-        );
-      },
-    }),
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: 'bottom',
-    animationEnabled: false,
-    swipeEnabled: false,
-  }
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                switch (routeName) {
+                case 'Images':
+                    iconName =
+                    Platform.OS === 'ios'
+                        ? `ios-camera${focused ? '' : '-outline'}`
+                        : 'md-camera';
+                    break;
+                case 'ForgeViewer':
+                    iconName = Platform.OS === 'ios' ? `ios-cloud-done${focused ? '' : '-outline'}` : 'md-cloud-done';
+                    break;
+                }
+                return (
+                    <Ionicons
+                        name={iconName}
+                        size={28}
+                        style={{ marginBottom: -3 }}
+                        color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+                    />
+                );
+            },
+        }),
+        tabBarComponent: TabBarBottom,
+        tabBarPosition: 'bottom',
+        animationEnabled: false,
+        swipeEnabled: false,
+    }
 );
