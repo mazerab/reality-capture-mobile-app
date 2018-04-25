1df7be4cc35177be35dcf55cf936a2519bd72e0a
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-25 11:22:54 -0700

    Update DataService.js

--- api/DataService.js
+++ api/DataService.js
@@ -1,14 +1,16 @@
 'use strict';
 
+import React from 'react';
 import { Alert } from 'react-native';
 
 import Config from '../constants/Config';
 import RedisService from './RedisService';
 import Utils from './Utils';
 
-export default class DataService {
+export default class DataService extends React.Component {
 
-    constructor() {
+    constructor(props) {
+        super(props);
         this.utils = new Utils();
     }
 
@@ -27,9 +29,10 @@ export default class DataService {
                 } else {
                     //this.utils.logResponseErrorToConsole('Failed to upload processed data to Autodesk Cloud!', res);
                     const message = JSON.parse(res._bodyText);
+                    console.info('message: ' + JSON.stringify(message));
                     Alert.alert(
                         'Failed to upload processed data to Autodesk Cloud!',
-                        message['ERROR'],
+                        JSON.stringify(message.ERROR),
                         [
                             {
                                 text: 'Cancel', 
