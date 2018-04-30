d1ec0631c6c8ee5e46ae92246f42a988751be91d
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-30 13:38:05 -0700

    Update DerivativeService.js

--- api/DerivativeService.js
+++ api/DerivativeService.js
@@ -1,59 +1,48 @@
 'use strict';
 
-import React from 'react';
-
 import Config from '../constants/Config';
-import Utils from './Utils';
+import {logRequestInfoToConsole, logResponseInfoToConsole, logResponseErrorToConsole, logFetchErrorToConsole} from './Utils';
 
-export default class DerivativeService extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.utils = new Utils();
-    }
-
-    async downloadBubbles() {
-        const endpoint = Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT + '/derivative/downloadBubbles';
-        const api = '/demo/derivative/downloadBubbles';
-        this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
-        return fetch(endpoint, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
+export const downloadBubbles = () => {
+    const endpoint = `${Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT}/derivative/downloadBubbles`;
+    const api = '/demo/derivative/downloadBubbles';
+    logRequestInfoToConsole(api, 'GET', endpoint, null);
+    return fetch(endpoint, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then((res) => {
+            logResponseInfoToConsole(api, 'GET', res);
+            if (res.ok) {
+                return res.json();
+            } else {
+                logResponseErrorToConsole(`Failed to download bubbles! ${JSON.stringify(res)}`);
+                alert('Failed to download bubbles!');
+            }
         })
-            .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'GET', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to download bubbles!' + JSON.stringify(res));
-                    alert('Failed to download bubbles!');
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
+        .catch((err) => {
+            logFetchErrorToConsole(err);
+        });
+};
 
-    async getManifest() {
-        const endpoint = Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT + '/derivative/getManifest';
-        const api = '/demo/derivative/getManifest';
-        this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
-        return fetch(endpoint, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
+export const getManifest = () => {
+    const endpoint = `${Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT}/derivative/getManifest`;
+    const api = '/demo/derivative/getManifest';
+    logRequestInfoToConsole(api, 'GET', endpoint, null);
+    return fetch(endpoint, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then((res) => {
+            logResponseInfoToConsole(api, 'GET', res);
+            if (res.ok) {
+                return res.json();
+            } else {
+                logResponseErrorToConsole('Failed to get manifest!', res);
+                alert('Failed to get manifest!');
+            }
         })
-            .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'GET', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to get manifest!', res);
-                    alert('Failed to get manifest!');
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
-
-}
+        .catch((err) => {
+            logFetchErrorToConsole(err);
+        });
+};
