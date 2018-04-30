2e7f9d8771a8560992e25653ffea5acefeb9baac
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-30 13:37:32 -0700

    Update DataService.js

--- api/DataService.js
+++ api/DataService.js
@@ -1,58 +1,26 @@
 'use strict';
 
-import React from 'react';
-import { Alert } from 'react-native';
-
 import Config from '../constants/Config';
-import RedisService from './RedisService';
-import Utils from './Utils';
+import {logRequestInfoToConsole, logResponseInfoToConsole, logResponseErrorToConsole, logFetchErrorToConsole} from './Utils';
 
-export default class DataService extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.utils = new Utils();
-    }
-
-    async uploadAndTranslateProcessedData() {
-        const endpoint = Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT + '/data/uploadAndTranslate';
-        const api = '/demo/data/uploadAndTranslate';
-        this.utils.logRequestInfoToConsole(api, 'POST', endpoint, null);
-        return fetch(endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' }
+export const uploadAndTranslateProcessedData = () => {
+    const endpoint = `${Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT}/data/uploadAndTranslate`;
+    const api = '/demo/data/uploadAndTranslate';
+    logRequestInfoToConsole(api, 'POST', endpoint, null);
+    return fetch(endpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then((res) => {
+            logResponseInfoToConsole(api, 'POST', res);
+            if (res.ok) {
+                return res.json();
+            } else {
+                logResponseErrorToConsole('Failed to upload processed data to Autodesk Cloud!', res);
+                alert('Failed to upload processed data to Autodesk Cloud!!');
+            }
         })
-            .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'POST', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    //this.utils.logResponseErrorToConsole('Failed to upload processed data to Autodesk Cloud!', res);
-                    const message = JSON.parse(res._bodyText);
-                    console.info('message: ' + JSON.stringify(message));
-                    Alert.alert(
-                        'Failed to upload processed data to Autodesk Cloud!',
-                        JSON.stringify(message.ERROR),
-                        [
-                            {
-                                text: 'Cancel', 
-                                onPress: () => { console.info('INFO: Hit Cancel button!'); } 
-                            },
-                            {
-                                text: 'Retry', 
-                                onPress: () => { 
-                                    this.RedisService = new RedisService();
-                                    this.RedisService.initBackend();
-                                }
-                            }
-                        ],
-                        { cancelable: false }
-                    );
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
-
-}
+        .catch((err) => {
+            logFetchErrorToConsole(err);
+        });
+};
