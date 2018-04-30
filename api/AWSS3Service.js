1938199d7e75f2052cb55565d804499c0ae37f2d
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-30 13:37:16 -0700

    Update AWSS3Service.js

--- api/AWSS3Service.js
+++ api/AWSS3Service.js
@@ -1,49 +1,39 @@
 'use strict';
 
-import React from 'react';
 import PathParse from 'path-parse';
-
 import Config from '../constants/Config';
 
-export default class AWSS3Service extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    async uploadImageToS3BucketAsync(imageFileUri, fileSize) {
-        const fileName = PathParse(imageFileUri).base;
-        const s3Url = Config.AWS_S3_BASE_ENDPOINT + '/' + Config.AWS_S3_BUCKET + '/' + fileName;
-        const photo = {
-            'name': fileName,
-            'type': 'image/jpeg',
-            'uri': imageFileUri
-        };
-        return new Promise(function(resolve, reject) {
-            const xhr = new XMLHttpRequest();
-            xhr.open('PUT', s3Url);
-            xhr.setRequestHeader('Content-Type', 'image/jpeg');
-            xhr.setRequestHeader('X-Amz-ACL', 'public-read');
-            xhr.onload = function() {
-                if (this.status >= 200 && this.status < 300) {
-                    console.info('INFO: File Uploaded to S3 at: ' + s3Url);
-                    resolve({ 's3Url': s3Url });
-                } else {
-                    console.error('ERROR: File upload failed. :( ' + fileName);
-                    reject({
-                        status: this.status,
-                        statusText: xhr.statusText
-                    });
-                }
-            };
-            xhr.onerror = function() {
+export const uploadImageToS3BucketAsync = (imageFileUri) => { 
+    const fileName = PathParse(imageFileUri).base;
+    const s3Url = `${Config.AWS_S3_BASE_ENDPOINT}/${Config.AWS_S3_BUCKET}/${fileName}`;
+    const photo = {
+        'name': fileName,
+        'type': 'image/jpeg',
+        'uri': imageFileUri
+    };
+    return new Promise(function(resolve, reject) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('PUT', s3Url);
+        xhr.setRequestHeader('Content-Type', 'image/jpeg');
+        xhr.setRequestHeader('X-Amz-ACL', 'public-read');
+        xhr.onload = function() {
+            if (this.status >= 200 && this.status < 300) {
+                console.info(`INFO: File Uploaded to S3 at: ${s3Url}`);
+                resolve({ 's3Url': s3Url });
+            } else {
+                console.error(`ERROR: File upload failed. :( ${fileName}`);
                 reject({
                     status: this.status,
                     statusText: xhr.statusText
                 });
-            };
-            xhr.send(photo);
-        });
-    }
-
-}
+            }
+        };
+        xhr.onerror = function() {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send(photo);
+    });
+};
