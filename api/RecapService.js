71dab80a9bb683a5abf5596b5ca159f76e39b802
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-30 13:39:52 -0700

    Update RecapService.js

--- api/RecapService.js
+++ api/RecapService.js
@@ -1,103 +1,92 @@
 'use strict';
 
-import React from 'react';
-
 import Config from '../constants/Config';
-import Utils from './Utils';
+import {logRequestInfoToConsole, logResponseInfoToConsole, logResponseErrorToConsole, logFetchErrorToConsole} from './Utils';
 
-export default class RecapService extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.utils = new Utils();
-    }
-
-    async getPhotoSceneLink() {
-        const endpoint = Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT + '/redis/photoscenelink';
-        const api = '/demo/redis/photoscenelink';
-        this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
-        return fetch(endpoint, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
+export const getPhotoSceneLink = () => {
+    const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/redis/photoscenelink`;
+    const api = '/demo/redis/photoscenelink';
+    logRequestInfoToConsole(api, 'GET', endpoint, null);
+    return fetch(endpoint, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then((res) => {
+            logResponseInfoToConsole(api, 'POST', res);
+            if (res.ok) {
+                return res.json();
+            } else {
+                logResponseErrorToConsole('Failed to get photoscenelink!', res);
+                alert('Failed to get photoscenelink!');
+            }
         })
-            .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'POST', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to get photoscenelink!', res);
-                    alert('Failed to get photoscenelink!');
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
+        .catch((err) => {
+            logFetchErrorToConsole(err);
+        });
+};
 
-    async pollProcessingStatus() {
-        const endpoint = Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT + '/redis/processingstatus';
-        const api = '/demo/redis/processingstatus';
-        this.utils.logRequestInfoToConsole(api, 'GET', endpoint, null);
-        return fetch(endpoint, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
+export const pollProcessingStatus = () => {
+    const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/redis/processingstatus`;
+    const api = '/demo/redis/processingstatus';
+    logRequestInfoToConsole(api, 'GET', endpoint, null);
+    return fetch(endpoint, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then((res) => {
+            logResponseInfoToConsole(api, 'POST', res);
+            if (res.ok) {
+                return res.json();
+            } else {
+                logResponseErrorToConsole('Failed to set processing status!', res);
+                alert('Failed to set processing status!');
+            }
         })
-            .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'POST', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to set processing status!', res);
-                    alert('Failed to set processing status!');
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
+        .catch((err) => {
+            logFetchErrorToConsole(err);
+        });
+};
 
-    async processPhotoScene() {
-        const endpoint = Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT + '/recap/processPhotoScene';
-        const api = '/demo/recap/processPhotoScene';
-        this.utils.logRequestInfoToConsole(api, 'POST', endpoint, null);
-        return fetch(endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' }
+export const processPhotoScene = () => {
+    const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/recap/processPhotoScene`;
+    const api = '/demo/recap/processPhotoScene';
+    logRequestInfoToConsole(api, 'POST', endpoint, null);
+    return fetch(endpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then((res) => {
+            logResponseInfoToConsole(api, 'POST', res);
+            if (res.ok) {
+                return res.json();
+            } else {
+                logResponseErrorToConsole('Failed to process Photoscene!', res);
+                alert('Failed to process Photoscene!');
+            }
         })
-            .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'POST', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to process Photoscene!', res);
-                    alert('Failed to process Photoscene!');
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
+        .catch((err) => {
+            logFetchErrorToConsole(err);
+        });
+};
 
-    async setProcessingStatusInProgress() {
-        const endpoint = Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT + '/redis/processingstatus?processingstatus=InProgress';
-        const api = '/demo/redis/processingstatus';
-        this.utils.logRequestInfoToConsole(api, 'POST', endpoint, null);
-        return fetch(endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' }
+export const setProcessingStatusInProgress = () => {
+    const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/redis/processingstatus?processingstatus=InProgress`;
+    const api = '/demo/redis/processingstatus';
+    logRequestInfoToConsole(api, 'POST', endpoint, null);
+    return fetch(endpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then((res) => {
+            logResponseInfoToConsole(api, 'POST', res);
+            if (res.ok) {
+                return res.json();
+            } else {
+                logResponseErrorToConsole('Failed to set processing status!', res);
+                alert('Failed to set processing status!');
+            }
         })
-            .then((res) => {
-                this.utils.logResponseInfoToConsole(api, 'POST', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.utils.logResponseErrorToConsole('Failed to set processing status!', res);
-                    alert('Failed to set processing status!');
-                }
-            })
-            .catch((err) => {
-                this.utils.logFetchErrorToConsole(err);
-            });
-    }
-
-}
+        .catch((err) => {
+            logFetchErrorToConsole(err);
+        });
+};
