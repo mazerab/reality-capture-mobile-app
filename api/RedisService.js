ff05c12d6185f54142c6fffcd06fd0207236d574
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-30 13:40:22 -0700

    Update RedisService.js

--- api/RedisService.js
+++ api/RedisService.js
@@ -1,57 +1,46 @@
 'use strict';
 
-import React from 'react';
-
 import Config from '../constants/Config';
-import Utils from './Utils';
+import {logRequestInfoToConsole, logResponseInfoToConsole, logResponseErrorToConsole, logFetchErrorToConsole} from './Utils';
 
-export default class RedisService extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.Utils = new Utils();
-    }
-
-    initBackend() {
-        const endpoint = Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT + '/redis/initSessionState';
-        const api = '/demo/redis/initSessionState';
-        this.Utils.logRequestInfoToConsole(api, 'POST', endpoint, null);
-        return fetch(endpoint, {
-            method: 'POST'
+export const initBackend = () => {
+    const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/redis/initSessionState`;
+    const api = '/demo/redis/initSessionState';
+    logRequestInfoToConsole(api, 'POST', endpoint, null);
+    return fetch(endpoint, {
+        method: 'POST'
+    })
+        .then((res) => {
+            logResponseInfoToConsole(api, 'POST', res);
+            if (res.ok) {
+                return res.json();
+            } else {
+                logResponseErrorToConsole('Failed to initialize backend!', res);
+                alert('Failed to create initialize backend!');
+            }
         })
-            .then((res) => {
-                this.Utils.logResponseInfoToConsole(api, 'POST', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.Utils.logResponseErrorToConsole('Failed to initialize backend!', res);
-                    alert('Failed to create initialize backend!');
-                }
-            })
-            .catch((err) => {
-                this.Utils.logFetchErrorToConsole(err);
-            });
-    }
+        .catch((err) => {
+            logFetchErrorToConsole(err);
+        });
+};
 
-    pushS3Url(uri) {
-        const endpoint = Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT + '/redis/imageUris?uri=' + uri;
-        const api = '/demo/redis/imageUris';
-        this.Utils.logRequestInfoToConsole(api, 'POST', endpoint, null);
-        return fetch(endpoint, {
-            method: 'POST'
+export const pushS3Url = (uri) => {
+    const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/redis/imageUris?uri=${uri}`;
+    const api = '/demo/redis/imageUris';
+    logRequestInfoToConsole(api, 'POST', endpoint, null);
+    return fetch(endpoint, {
+        method: 'POST'
+    })
+        .then((res) => {
+            logResponseInfoToConsole(api, 'POST', res);
+            if (res.ok) {
+                return res.json();
+            } else {
+                logResponseErrorToConsole('Failed to initialize backend!', res);
+                alert('Failed to create initialize backend!');
+            }
         })
-            .then((res) => {
-                this.Utils.logResponseInfoToConsole(api, 'POST', res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    this.Utils.logResponseErrorToConsole('Failed to initialize backend!', res);
-                    alert('Failed to create initialize backend!');
-                }
-            })
-            .catch((err) => {
-                this.Utils.logFetchErrorToConsole(err);
-            });
-    }
-    
-}
+        .catch((err) => {
+            logFetchErrorToConsole(err);
+        });
+};
