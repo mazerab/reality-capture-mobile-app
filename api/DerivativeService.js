b015c74b87822df40284342e13edba689e5a9386
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-07-27 17:12:46 -0700

    Update DerivativeService.js

--- api/DerivativeService.js
+++ api/DerivativeService.js
@@ -1,46 +1,46 @@
-'use strict';
+'use strict'
 
-import Config from '../constants/Config';
-import {logRequestInfoToConsole, logResponseInfoToConsole, logFetchErrorToConsole} from './Utils';
+import Config from '../constants/Config'
+import {logRequestInfoToConsole, logResponseInfoToConsole, logFetchErrorToConsole} from './Utils'
 
 export const downloadBubbles = () => {
-    const endpoint = `${Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT}/derivative/downloadBubbles`;
-    const api = '/demo/derivative/downloadBubbles';
-    logRequestInfoToConsole(api, 'GET', endpoint, null);
-    return fetch(endpoint, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
+  const endpoint = `${Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT}/derivative/downloadBubbles`
+  const api = '/demo/derivative/downloadBubbles'
+  logRequestInfoToConsole(api, 'GET', endpoint, null)
+  return fetch(endpoint, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((res) => {
+      logResponseInfoToConsole(api, 'GET', res)
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw Error('Failed to download bubbles!')
+      }
     })
-        .then((res) => {
-            logResponseInfoToConsole(api, 'GET', res);
-            if (res.ok) {
-                return res.json();
-            } else {
-                throw Error('Failed to download bubbles!');
-            }
-        })
-        .catch((err) => {
-            logFetchErrorToConsole(err);
-        });
-};
+    .catch((err) => {
+      logFetchErrorToConsole(err)
+    })
+}
 
 export const getManifest = () => {
-    const endpoint = `${Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT}/derivative/getManifest`;
-    const api = '/demo/derivative/getManifest';
-    logRequestInfoToConsole(api, 'GET', endpoint, null);
-    return fetch(endpoint, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
+  const endpoint = `${Config.AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT}/derivative/getManifest`
+  const api = '/demo/derivative/getManifest'
+  logRequestInfoToConsole(api, 'GET', endpoint, null)
+  return fetch(endpoint, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then((res) => {
+      logResponseInfoToConsole(api, 'GET', res)
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw Error('Failed to get manifest!')
+      }
     })
-        .then((res) => {
-            logResponseInfoToConsole(api, 'GET', res);
-            if (res.ok) {
-                return res.json();
-            } else {
-                throw Error('Failed to get manifest!');
-            }
-        })
-        .catch((err) => {
-            logFetchErrorToConsole(err);
-        });
-};
+    .catch((err) => {
+      logFetchErrorToConsole(err)
+    })
+}
