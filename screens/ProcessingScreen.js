00c06edc52f8a1b70448048a19e3505920ae6b86
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-07-27 17:07:50 -0700

    Update ProcessingScreen.js

--- screens/ProcessingScreen.js
+++ screens/ProcessingScreen.js
@@ -1,29 +1,29 @@
-import React, {Component} from 'react';
-import { ActivityIndicator, Modal, StyleSheet, View } from 'react-native';
+import React from 'react'
+import { ActivityIndicator, Modal, StyleSheet, View } from 'react-native'
 
 const ProcessingScreen = props => {
-    const { 
-        processing,
-        ...attributes
-    } = props;
-    return(
-        <Modal
-            transparent={true}
-            animationType={'none'}
-            visible={processing}
-            onRequestClose={ () => { console.info('close modal.') } }>
-            <View style={styles.modalBackground}>
-                <View style={styles.activityIndicatorWrapper}>
-                    <ActivityIndicator animating={processing} />
-                </View>
-            </View>
-        </Modal>
-    );
+  const {
+    processing,
+    ...attributes
+  } = props
+  return (
+    <Modal
+      transparent
+      animationType={'none'}
+      visible={processing}
+      onRequestClose={() => { console.info('close modal.') }}>
+      <View style={styles.modalBackground}>
+        <View style={styles.activityIndicatorWrapper}>
+          <ActivityIndicator animating={processing} />
+        </View>
+      </View>
+    </Modal>
+  )
 }
 
 const styles = StyleSheet.create({
   modalBackground: {
-        flex: 1,
+    flex: 1,
     alignItems: 'center',
     flexDirection: 'column',
     justifyContent: 'space-around',
@@ -38,6 +38,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around'
   }
-});
+})
 
-export default ProcessingScreen;
+export default ProcessingScreen
