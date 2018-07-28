3225d697dbc3a4151c8c3e9548079464240ec843
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-07-27 17:14:12 -0700

    Update RedisService.js

--- api/RedisService.js
+++ api/RedisService.js
@@ -1,44 +1,44 @@
-'use strict';
+'use strict'
 
-import Config from '../constants/Config';
-import {logRequestInfoToConsole, logResponseInfoToConsole, logFetchErrorToConsole} from './Utils';
+import Config from '../constants/Config'
+import {logRequestInfoToConsole, logResponseInfoToConsole, logFetchErrorToConsole} from './Utils'
 
 export const initBackend = () => {
-    const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/redis/initSessionState`;
-    const api = '/demo/redis/initSessionState';
-    logRequestInfoToConsole(api, 'POST', endpoint, null);
-    return fetch(endpoint, {
-        method: 'POST'
+  const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/redis/initSessionState`
+  const api = '/demo/redis/initSessionState'
+  logRequestInfoToConsole(api, 'POST', endpoint, null)
+  return fetch(endpoint, {
+    method: 'POST'
+  })
+    .then((res) => {
+      logResponseInfoToConsole(api, 'POST', res)
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw Error('Failed to initialize backend!')
+      }
     })
-        .then((res) => {
-            logResponseInfoToConsole(api, 'POST', res);
-            if (res.ok) {
-                return res.json();
-            } else {
-                throw Error('Failed to initialize backend!');
-            }
-        })
-        .catch((err) => {
-            logFetchErrorToConsole(err);
-        });
-};
+    .catch((err) => {
+      logFetchErrorToConsole(err)
+    })
+}
 
 export const pushS3Url = (uri) => {
-    const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/redis/imageUris?uri=${uri}`;
-    const api = '/demo/redis/imageUris';
-    logRequestInfoToConsole(api, 'POST', endpoint, null);
-    return fetch(endpoint, {
-        method: 'POST'
+  const endpoint = `${Config.AWS_RECAP_LAMBDA_BASE_ENDPOINT}/redis/imageUris?uri=${uri}`
+  const api = '/demo/redis/imageUris'
+  logRequestInfoToConsole(api, 'POST', endpoint, null)
+  return fetch(endpoint, {
+    method: 'POST'
+  })
+    .then((res) => {
+      logResponseInfoToConsole(api, 'POST', res)
+      if (res.ok) {
+        return res.json()
+      } else {
+        throw Error('Failed to add image to the photoscene!')
+      }
     })
-        .then((res) => {
-            logResponseInfoToConsole(api, 'POST', res);
-            if (res.ok) {
-                return res.json();
-            } else {
-                throw Error('Failed to add image to the photoscene!');
-            }
-        })
-        .catch((err) => {
-            logFetchErrorToConsole(err);
-        });
-};
+    .catch((err) => {
+      logFetchErrorToConsole(err)
+    })
+}
