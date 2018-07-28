40a92678b839eafe27d369cfe8dcdafdff40e2c7
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-07-27 17:11:17 -0700

    Update Config.js

--- constants/Config.js
+++ constants/Config.js
@@ -1,26 +1,26 @@
-'use strict';
+'use strict'
 
 module.exports = {
 
-    FORGE_APP_ID: '<your Forge App ID goes here...>',
+  FORGE_APP_ID: 's85YumBg3IrVYIdiIwlsjHGDxlMKkGJp',
 
-    AWS_RECAP_LAMBDA_BASE_ENDPOINT: 'https://<recap lambda>.execute-api.us-east-1.amazonaws.com/demo',
-    AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT: 'https://<upload and translate lambda>.execute-api.us-east-1.amazonaws.com/demo',
-    AWS_S3_BASE_ENDPOINT: 'https://s3.amazonaws.com',
-    AWS_S3_BUCKET: '<name of your S3 bucket>',
+  AWS_RECAP_LAMBDA_BASE_ENDPOINT: 'https://<your recap server url>.execute-api.us-east-1.amazonaws.com/demo',
+  AWS_UPLOAD_TRANSLATE_LAMBDA_BASE_ENDPOINT: 'https://<your translation server url>.execute-api.us-east-1.amazonaws.com/demo',
+  AWS_S3_BASE_ENDPOINT: 'https://s3.amazonaws.com',
+  AWS_S3_BUCKET: '<your bucket name>',
 
-    IOS_SIMULATOR_TESTING: true,
+  PUSH_NOTIFICATION_DISABLED: true,
 
-    // Do not edit below this line ...
-    OAUTH_BASE_ENDPOINT: 'https://developer.api.autodesk.com/authentication/v1',
+  // Do not edit below this line ...
+  OAUTH_BASE_ENDPOINT: 'https://developer.api.autodesk.com/authentication/v1',
 
-    scopePublic: ['viewables:read'],
+  scopePublic: ['viewables:read'],
 
-    // Available formats are rcm (Autodesk Recap Photo Mesh), 
-    // rcs (Autodesk Recap Point Cloud) and obj (Wavefront object).
-    format: 'obj',
-    sceneName: 'reality-capture-mobile-app',
-    sceneType: 'object',
-    sceneCallback: '/recap/scene/callback',
+  // Available formats are rcm (Autodesk Recap Photo Mesh),
+  // rcs (Autodesk Recap Point Cloud) and obj (Wavefront object).
+  format: 'obj',
+  sceneName: 'reality-capture-mobile-app',
+  sceneType: 'object',
+  sceneCallback: '/recap/scene/callback'
 
-};
+}
