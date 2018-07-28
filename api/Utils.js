45d888d74ae040c9dd91ad79172ca36b7e454f79
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-07-27 17:14:27 -0700

    Update Utils.js

--- api/Utils.js
+++ api/Utils.js
@@ -1,25 +1,25 @@
-'use strict';
+'use strict'
 
 export const logRequestInfoToConsole = (endpoint, httpMethod, url, body) => {
-    if (body) {
-        console.info(`INFO: ${httpMethod} ${endpoint} : Request: ${url} : Payload: ${JSON.stringify(body)}`);
-    } else {
-        console.info(`INFO: ${httpMethod} ${endpoint} : Request: ${url}`);
-    }  
-};
+  if (body) {
+    console.info(`INFO: ${httpMethod} ${endpoint} : Request: ${url} : Payload: ${JSON.stringify(body)}`)
+  } else {
+    console.info(`INFO: ${httpMethod} ${endpoint} : Request: ${url}`)
+  }
+}
 
 export const logResponseInfoToConsole = (endpoint, httpMethod, response) => {
-    if (response) {
-        console.info(`INFO: ${httpMethod} ${endpoint}: Response: ${JSON.stringify(response)}`);
-    } else {
-        console.info(`INFO: ${httpMethod} ${endpoint}: Response: none`);
-    }  
-};
+  if (response) {
+    console.info(`INFO: ${httpMethod} ${endpoint}: Response: ${JSON.stringify(response)}`)
+  } else {
+    console.info(`INFO: ${httpMethod} ${endpoint}: Response: none`)
+  }
+}
 
 export const logFetchErrorToConsole = (error) => {
-    if (error) {
-        console.error(`ERROR: Fetch failed with error: ${error}`);
-    } else {
-        console.error('ERROR: Fetch failed with unexpected error!');
-    }
-};
+  if (error) {
+    console.error(`ERROR: Fetch failed with error: ${error}`)
+  } else {
+    console.error('ERROR: Fetch failed with unexpected error!')
+  }
+}
